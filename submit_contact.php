f3283b50abd852ae6b57c981e5d7fa17953e4d1a
jamesattoh <koss.attoh27@gmail.com>
2024-10-10 12:08:34 +0100

    PDO configuration for database access

--- submit_contact.php
+++ submit_contact.php
@@ -8,18 +8,18 @@
 $postData = $_POST;
 
 if (
-    !isset($postData['email']) //isset() verifie si une variable est definie ou 
-    || !filter_var($postData['email'], FILTER_VALIDATE_EMAIL) //validité de l'email ou non
+    !isset($postData['email'])
+    || !filter_var($postData['email'], FILTER_VALIDATE_EMAIL)
     || empty($postData['message'])
-    || trim($postData['message']) === '' //supprime l'espace ou autre caractere(à preciser) en début et en fin de chaîne
+    || trim($postData['message']) === ''
 ) {
-    echo ('Il faut un email et un message valides pour soumettre le formulaire.');
+    echo('Il faut un email et un message valides pour soumettre le formulaire.');
     return;
 }
 
 $isFileLoaded = false;
 // Testons si le fichier a bien été envoyé et s'il n'y a pas des erreurs
-if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {  //$_FILES est le array qui recevra les fichiers envoyés
+if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {
     // Testons, si le fichier est trop volumineux
     if ($_FILES['screenshot']['size'] > 1000000) {
         echo "L'envoi n'a pas pu être effectué, erreur ou image trop volumineuse";
@@ -27,7 +27,7 @@ if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {  //$
     }
 
     // Testons, si l'extension n'est pas autorisée
-    $fileInfo = pathinfo($_FILES['screenshot']['name']); //pathinfo verifie l'extension des fichiers envoyés
+    $fileInfo = pathinfo($_FILES['screenshot']['name']);
     $extension = $fileInfo['extension'];
     $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
     if (!in_array($extension, $allowedExtensions)) {
@@ -37,21 +37,19 @@ if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {  //$
 
     // Testons, si le dossier uploads est manquant
     $path = __DIR__ . '/uploads/';
-    if (!is_dir($path)) { //is_dir verifie si le fichier (filename) est un dossier
+    if (!is_dir($path)) {
         echo "L'envoi n'a pas pu être effectué, le dossier uploads est manquant";
         return;
     }
 
     // On peut valider le fichier et le stocker définitivement
-    move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . basename($_FILES['screenshot']['name'])); //move_uploaded_file conserve les fichiers téléversés sur mon serveur : move_uploaded_file(from, to)
+    move_uploaded_file($_FILES['screenshot']['tmp_name'], $path . basename($_FILES['screenshot']['name']));
     $isFileLoaded = true;
 }
-
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -59,7 +57,6 @@ if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {  //$
     <title>Site de Recettes - Contact reçu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
     <div class="container">
 
@@ -69,16 +66,15 @@ if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] === 0) {  //$
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">Rappel de vos informations</h5>
-                <p class="card-text"><b>Email</b> : <?php echo ($postData['email']); ?></p>
-                <p class="card-text"><b>Message</b> : <?php echo (strip_tags($postData['message'])); ?></p> <!--htmlspecialchars peut être aussi utilisé a la place de strip-tags -->
+                <p class="card-text"><b>Email</b> : <?php echo($postData['email']); ?></p>
+                <p class="card-text"><b>Message</b> : <?php echo(strip_tags($postData['message'])); ?></p>
                 <?php if ($isFileLoaded) : ?>
                     <div class="alert alert-success" role="alert">
                         L'envoi a bien été effectué !
                     </div>
-                <?php endif; ?>           
+                <?php endif; ?>
             </div>
         </div>
     </div>
 </body>
-
 </html>
