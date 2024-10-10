78668ceafb8613503f54ed741739ee7a50a2fbe4
jamesattoh <koss.attoh27@gmail.com>
2024-10-10 18:28:41 +0100

    trying to manage comments - previousCommitIsTheLastOneOfTheProject

--- recipes_update.php
+++ recipes_update.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require_once(__DIR__ . '/isConnect.php');
 require_once(__DIR__ . '/config/mysql.php');
 require_once(__DIR__ . '/databaseconnect.php');
 
@@ -11,21 +12,17 @@ require_once(__DIR__ . '/databaseconnect.php');
 $getData = $_GET;
 
 if (!isset($getData['id']) || !is_numeric($getData['id'])) {
-    echo('La recette n\'existe pas');
+    echo('Il faut un identifiant de recette pour la modifier.');
     return;
 }
 
-// On récupère la recette
-$retrieveRecipeStatement = $mysqlClient->prepare('SELECT r.* FROM recipes r WHERE r.recipe_id = :id ');
+$retrieveRecipeStatement = $mysqlClient->prepare('SELECT * FROM recipes WHERE recipe_id = :id');
 $retrieveRecipeStatement->execute([
     'id' => (int)$getData['id'],
 ]);
-$recipe = $retrieveRecipeStatement->fetch();
+$recipe = $retrieveRecipeStatement->fetch(PDO::FETCH_ASSOC);
 
-if (!$recipe) {
-    echo('La recette n\'existe pas');
-    return;
-}
+// si la recette n'est pas trouvée, renvoyer un message d'erreur
 ?>
 
 <!DOCTYPE html>
@@ -34,7 +31,7 @@ if (!$recipe) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de Recettes - <?php echo($recipe['title']); ?></title>
+    <title>Site de Recettes - Edition de recette</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet"
@@ -44,16 +41,26 @@ if (!$recipe) {
     <div class="container">
 
         <?php require_once(__DIR__ . '/header.php'); ?>
-        <h1><?php echo($recipe['title']); ?></h1>
-        <div class="row">
-            <article class="col">
-                <?php echo($recipe['recipe']); ?>
-            </article>
-            <aside class="col">
-                <p><i>Contribuée par <?php echo($recipe['author']); ?></i></p>
-            </aside>
-        </div>
+        <h1>Mettre à jour <?php echo($recipe['title']); ?></h1>
+        <form action="recipes_post_update.php" method="POST">
+            <div class="mb-3 visually-hidden">
+                <label for="id" class="form-label">Identifiant de la recette</label>
+                <input type="hidden" class="form-control" id="id" name="id" value="<?php echo($getData['id']); ?>">
+            </div>
+            <div class="mb-3">
+                <label for="title" class="form-label">Titre de la recette</label>
+                <input type="text" class="form-control" id="title" name="title" aria-describedby="title-help" value="<?php echo($recipe['title']); ?>">
+                <div id="title-help" class="form-text">Choisissez un titre percutant !</div>
+            </div>
+            <div class="mb-3">
+                <label for="recipe" class="form-label">Description de la recette</label>
+                <textarea class="form-control" placeholder="Seulement du contenu vous appartenant ou libre de droits." id="recipe" name="recipe"><?php echo $recipe['recipe']; ?></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+        <br />
     </div>
+
     <?php require_once(__DIR__ . '/footer.php'); ?>
 </body>
 </html>
